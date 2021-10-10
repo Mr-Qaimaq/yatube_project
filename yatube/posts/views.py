4aa768d35af0cb2d781ccd53c8da6d22a44cda7b
Samgar Kali <kali2003@inbox.ru>
2021-10-10 01:09:48 -0400

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Тут есть фильтр {slug}')
